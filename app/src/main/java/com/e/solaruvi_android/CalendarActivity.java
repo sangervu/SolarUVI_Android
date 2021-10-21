32ebf244523459c252997e7f877bc6641a28160b
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2021-10-21 20:19:36 +0300

    kalenterivalinta

--- app/src/main/java/com/e/solaruvi_android/CalendarActivity.java
+++ app/src/main/java/com/e/solaruvi_android/CalendarActivity.java
@@ -3,9 +3,14 @@ package com.e.solaruvi_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import com.e.solaruvi_android.calculations.SolarCalculations;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -14,7 +19,15 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        //TextView dataText1 = (TextView) findViewById(R.id.calendarView);
+        CalendarView simpleCalendarView = (CalendarView) findViewById(R.id.calendarView); // get the reference of CalendarView
+        long selectedDate = simpleCalendarView.getDate(); // get selected date in millisecond
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = formatter.format(new Date(selectedDate));
+
+        TextView dataText1 = (TextView) findViewById(R.id.editTextDate);
+
+        dataText1.setText(dateString);
 
     }
 }
